15428359b0d8f17ba5e3d700069efea3447a633e
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-03-08 12:05:55 +0200

    made layout pages

--- src/app/(pages)/(protected)/layout.tsx
+++ src/app/(pages)/(protected)/layout.tsx
@@ -1,11 +1,22 @@
+import { Footer, Navbar } from "@/components";
+
 export default function ProtectedLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-full min-h-screen h-full bg-amber-100 text-black">
-      {children}
+    <div className="body bg-foreground">
+      <Navbar
+        links={[
+          { href: "/", label: "Browse" },
+          { href: "/about", label: "All Recipes" },
+        ]}
+      />
+      <main className="main">
+        <div>{children}</div>
+      </main>
+      {Footer}
     </div>
   );
 }
