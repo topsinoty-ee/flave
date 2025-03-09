5fbbbc1e4a5769659e1902d7cda212ce9946f36f
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-03-09 23:42:12 +0200

    made section header support icons

--- src/components/section-header.tsx
+++ src/components/section-header.tsx
@@ -1,15 +1,20 @@
 export interface SectionHeaderProps {
+  icon?: React.ReactNode;
   title: string;
   description?: string;
 }
 
 export const SectionHeader: React.FC<SectionHeaderProps> = ({
+  icon,
   title,
   description,
 }) => (
   <div className="flex flex-col gap-1">
     <div className="flex w-full gap-2.5 items-baseline">
-      <h2 className="uppercase w-max text-nowrap">{title}</h2>
+      <div className="flex gap-2.5 items-center">
+        {icon}
+        <h2 className="uppercase w-max text-nowrap">{title}</h2>
+      </div>
       <span className="w-full border-b-4 select-none border-yellow" />
     </div>
     <p className="font-medium text-sm text-gray">{description}</p>
