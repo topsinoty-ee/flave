539283332c5281d07d0d17382a0b16d59a9f94d4
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-03-10 20:16:51 +0200

    feat: :construction: made tag image component and collage builder component...needs work

---
+++ src/app/api/collage/route.ts
@@ -0,0 +1,13 @@
+import { createCollage } from "@/util";
+
+export async function POST(req: Request) {
+  try {
+    const { images, options } = await req.json();
+
+    const collage = await createCollage(images, options);
+    return Response.json({ dataUrl: collage });
+  } catch (err) {
+    console.error(err);
+    return new Response("Failed to create collage", { status: 500 });
+  }
+}
