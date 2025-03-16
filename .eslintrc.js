fbf5a5cd2a8e1e33a451f428042c16bb1165e43d
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-03-16 23:07:06 +0200

    added some plugins and dep-check

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint, perfectionist"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    'next/core-web-vitals',
+    'plugin:unicorn/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:jsx-a11y/recommended'
   ],
   parserOptions: {
     project: "./tsconfig.json",
@@ -13,5 +17,11 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "error",
     "@typescript-eslint/explicit-function-return-type": "error",
     "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+    'perfectionist/sort-imports': 'error',
+    'unicorn/no-array-reduce': 'error',
+    'unicorn/prefer-module': 'off',
+    'sonarjs/no-duplicate-string': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'error'
   },
 };
