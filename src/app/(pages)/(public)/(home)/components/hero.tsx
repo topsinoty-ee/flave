a9d0d7a9598d400bcbba8e143f5044c1f2971a38
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-03-31 02:07:27 +0300

    changed heroes

--- src/app/(pages)/(public)/(home)/components/hero.tsx
+++ src/app/(pages)/(public)/(home)/components/hero.tsx
@@ -3,7 +3,7 @@ import { Image } from "@/components";
 export const Hero = () => {
   return (
     <section className="h-[calc(100vh+2rem)] flex w-full section bg-foreground -mt-5">
-      <div className="w-full h-[calc(90%-28px)] bg-black grid grid-cols-3 grid-rows-2">
+      {/* <div className="w-full h-[calc(90%-28px)] bg-black grid grid-cols-3 grid-rows-2">
         <Image src="/NEW-HERO-IMAGE-TOP-1.png" fill priority alt="" />
         <Image
           src="/NEW-HERO-IMAGE-TOP-2.png"
@@ -12,12 +12,7 @@ export const Hero = () => {
           priority
           alt=""
         />
-
-        {/* <div className="bg-black w-full h-full flex flex-col gap-5 p-10 -mt-20">
-          <h1 className="lowercase text-8xl leading-snug0  font-extrabold">
-            time, money & food.
-          </h1>
-        </div> */}
+        
         <Image src="/NEW-HERO-IMG-TOP-3.jpg" fill priority alt="" />
         <div className="bg-black w-full h-full flex flex-col gap-5 p-10">
           <h4>Am i weird?</h4>
@@ -28,7 +23,8 @@ export const Hero = () => {
           <h4>Am i weird?</h4>
           <h6>Yes or No</h6>
         </div>
-      </div>
+      </div> */}
+      <div className="home-hero-bg w-full h-[calc(90%-28px)] bg-black bg-no-repeat bg-contain" />
     </section>
   );
 };
--- src/app/(pages)/(public)/recipes/browse/_components/hero.tsx
+++ src/app/(pages)/(public)/recipes/browse/_components/hero.tsx
@@ -1,8 +1,9 @@
+import { Image } from "@/components";
 import { TextMask } from "@/components/textMask";
 
 export const Hero = () => (
   <section className="w-full browse-hero-bg flex flex-col justify-end -mt-15 p-20 h-[calc(100vh-2rem)]">
-    <TextMask
+    {/* <TextMask
       backgroundImage="/yellow-pattern-bg.png"
       fontFamily="Plus Jakarta Sans"
       fallbackColor="var(--color-foreground)"
@@ -12,6 +13,6 @@ export const Hero = () => (
     >
       <h1 className="text-9xl font-extrabold">Browse</h1>
       <h3>catering to your tastes.</h3>
-    </TextMask>
+    </TextMask> */}
   </section>
 );
