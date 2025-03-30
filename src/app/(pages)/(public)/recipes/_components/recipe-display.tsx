3bef4db103a27096d6b483b58e7ae06d80510ca8
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-03-31 00:31:39 +0300

    made recipe display block

---
+++ src/app/(pages)/(public)/recipes/_components/recipe-display.tsx
@@ -0,0 +1,10 @@
+import { DisplayResource, RecipeCard } from "@/components";
+
+export const RecipesDisplayBlock = async ({}) => {
+  return (
+    <>
+      <section></section>
+      <DisplayResource Component={RecipeCard} />
+    </>
+  );
+};
