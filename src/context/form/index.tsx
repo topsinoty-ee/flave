cdc57c89a45d0b3570e178dcf410b7ebe5d1ce85
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-04-10 00:52:44 +0300

    added state tracker to work only for dev

--- src/context/form/index.tsx
+++ src/context/form/index.tsx
@@ -113,7 +113,9 @@ export const FormProvider = <Schema extends ZodObject<ZodRawShape>>({
               {state.message}
             </small>
           )}
-          {/* <pre>{JSON.stringify(state)}</pre> */}
+          {process.env.NODE_ENV === "development" ? (
+            <pre>{JSON.stringify(state, null, 2)}</pre>
+          ) : null}
         </form>
       </RHFProvider>
     </FormContext.Provider>
