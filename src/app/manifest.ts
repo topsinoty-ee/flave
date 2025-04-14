2f13257877c24f80b912eef0596dff2de3aacd9d
Promise Temitope <oluwatobilobatemi05@gmail.com>
2025-04-14 22:05:11 +0300

    added manifest and seo stuff

---
+++ src/app/manifest.ts
@@ -0,0 +1,28 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Flave",
+    short_name: "Flave",
+    description: "A recipe search engine",
+    start_url: "/browse",
+    lang: "en",
+    dir: "auto",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    theme_color: "#f5c518",
+    background_color: "#ffffff",
+    display: "standalone",
+    orientation: "portrait-secondary",
+  };
+}
